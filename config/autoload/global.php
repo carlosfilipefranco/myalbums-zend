7af2fb8724bd294d7f0df8852e99b353c9508dc9
Carlos Franco <carlosfranco@wks-pvz-045.grupopie.pt.corp.lan>
2020-03-29 22:02:13 +0100

    Remove local socket

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=myalbums',
+        'dsn'            => 'dbname=myalbums',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         )
