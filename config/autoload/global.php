b377500f5ed1a316e9f91c48d8b08483a6229b80
Carlos Franco <carlosfranco@wks-pvz-045.grupopie.pt.corp.lan>
2020-03-29 22:09:24 +0100

    Fix database dsn

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'dbname=myalbums',
+        'dsn'            => 'mysql:dbname=myalbums;host=127.0.0.1',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         )
